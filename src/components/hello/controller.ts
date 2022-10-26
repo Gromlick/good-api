9f64795c8ef5cd2b4e75a973c193d092399e454c
Roz <gromlick@gmail.com>
2022-10-26 12:29:26 -0400

    add head method

--- src/components/hello/controller.ts
+++ src/components/hello/controller.ts
@@ -8,11 +8,20 @@ class HelloController {
     constructor (private readonly helloService: HelloService) {}
     route (app: Router) {
         const route = Router();
+        const body = this.helloService.getMessage();
+        const headers = {
+            'content-type': 'text/plain',
+            'content-length': body.length,
+        };
     
         app.use('/hello', route);
 
         route.get('/', mid.logRequest, (req, res) => {
-            res.send(this.helloService.getMessage()).status(200);
+            res.send(body).status(200).set(headers);
+        });
+
+        route.head('/', mid.logRequest, (req, res) => {
+            res.status(200).set(headers);
         });
     }
 }
